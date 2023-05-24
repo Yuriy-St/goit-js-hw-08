376926013319a905a0f5acbbc2c3eba3798708d4
Yuriy-St <yuriystaynov@gmail.com>
2023-05-24 15:09:13 +0300

    gallery page

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css'
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.body.querySelector('.gallery');
+galleryEl.innerHTML = galleryItems.reduce((htmlGallery, item) => {
+  return (
+    htmlGallery +
+    `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>
+    `
+  );
+}, '');
 
+const lightbox = new simpleLightbox(`.gallery a`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
