f6d8ca16e684b969f0d45fe12364bf0245faa7e6
Yuriy-St <yuriystaynov@gmail.com>
2023-06-04 22:14:43 +0300

    feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import lodash from 'lodash';
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = formEl.elements['email'];
+const messageEl = formEl.elements['message'];
+
+let currentValues;
+const savedValues = localStorage.getItem('feedback-form-state');
+
+if (savedValues) {
+  try {
+    currentValues = JSON.parse(savedValues);
+    emailEl.value = currentValues.email;
+    messageEl.value = currentValues.message;
+  } catch (e) {
+    console.error(e);
+  }
+} else {
+  formClear();
+}
+
+formEl.addEventListener('input', lodash.throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+function onInput() {
+  currentValues = {
+    email: emailEl.value,
+    message: messageEl.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentValues));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(currentValues);
+  formClear();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function formClear() {
+  emailEl.value = '';
+  messageEl.value = '';
+}
