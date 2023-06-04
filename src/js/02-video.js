442939ec42d7d9bdd09c6102426fc46f868d4659
Yuriy-St <yuriystaynov@gmail.com>
2023-06-04 21:25:12 +0300

    video page

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import lodash from 'lodash';
 
+const player = new Player('vimeo-player');
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  try {
+    currentTime = JSON.parse(currentTime);
+    player.setCurrentTime(currentTime);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+player.on('timeupdate', lodash.throttle(updatePlayerTime, 1000));
+
+function updatePlayerTime() {
+  player
+    .getCurrentTime()
+    .then(seconds => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
